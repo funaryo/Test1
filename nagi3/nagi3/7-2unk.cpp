393d10bcfcd29a7a5b6a7b89fbcb464500ea2c9c
funaryo <qqt274kd@extra.ocn.ne.jp>
2014-05-21 06:40:19 +0900

    tset
    awe

---
+++ nagi3/nagi3/7-2unk.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define BUFFER_SIZE 100
+
+int main(){
+	char ch[BUFFER_SIZE]= "Kansai University";
+	char ch2[BUFFER_SIZE+1] = "\0";
+	char a = '*';
+	int i = 0, j = 0;
+	int b = 0;
+
+	printf("挿入前の文字列: %s\n",ch);
+	printf("どの位置の文字の前に挿入しますか？\n");
+	scanf("%d", &b);	
+	
+	while( ch[i] != '\0'){
+		
+		
+		if( j == b ){
+			ch2[j] = a;
+			j++;
+		}else{
+			ch2[j] = ch[i]; 
+			i++;
+			j++;
+		}
+	}
+	printf("挿入後の文字列:%s\n", ch2);
+	scanf("%d", &b);	
+    return 0;
+}
